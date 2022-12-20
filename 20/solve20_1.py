20b34d6737839e8b0a8502763c65c82beb3b4b1e
Alexandre Hideki Hagihara <alexandre.hagihara@gmail.com>
2022-12-20 13:29:53 -0300

    Parte 1 do dia 20 foi (está lento)

--- 20/solve20_1.py
+++ 20/solve20_1.py
@@ -3,34 +3,47 @@ f = open(f"{os.path.dirname(__file__)}/input20.txt", "r")
 # f = open(f"{os.path.dirname(__file__)}/example20.txt", "r")
 
 encrypted_file = []
-flag_pass = []
+original_pos = []
+len_file = 0
 
 for line in f.readlines():
     encrypted_file.append(int(line.strip()) )
-    flag_pass.append(False)
+    original_pos.append(len_file)
+    len_file += 1
 
-while(False in flag_pass):
+for p in range(len_file):
     # print(encrypted_file)
-    for i in range(len(flag_pass)):
-        if not flag_pass[i]:
-            offset = encrypted_file[i]
-            new_pos = offset + i
-            if( new_pos > len(encrypted_file) ):
-                new_pos = (new_pos + 1) % len(encrypted_file)
-            elif( new_pos < 1 ):
-                new_pos = (new_pos - 1 + len(encrypted_file)) % len(encrypted_file)
-            
-            encrypted_file = encrypted_file[:i] + encrypted_file[i+1:]
-            encrypted_file = encrypted_file[:new_pos] + [offset] + encrypted_file[new_pos:]
-            flag_pass = flag_pass[:i] + flag_pass[i+1:]
-            flag_pass = flag_pass[:new_pos] + [True] + flag_pass[new_pos:]
-            break
+    i = original_pos.index(p)
+    offset = encrypted_file[i]
+    if( offset > 0 ):
+        old_pos = i
+        for j in range(offset):
+            new_pos = ( old_pos + 1 ) % len_file
+            tempV = encrypted_file[new_pos]
+            encrypted_file[new_pos] = encrypted_file[old_pos]
+            encrypted_file[old_pos] = tempV
+            tempP = original_pos[new_pos]
+            original_pos[new_pos] = original_pos[old_pos]
+            original_pos[old_pos] = tempP
+            old_pos = new_pos
+    elif( offset < 0 ):
+        old_pos = i
+        for j in range(-offset):
+            new_pos = ( old_pos - 1 + 2*len_file) % len_file
+            tempV = encrypted_file[new_pos]
+            encrypted_file[new_pos] = encrypted_file[old_pos]
+            encrypted_file[old_pos] = tempV
+            tempP = original_pos[new_pos]
+            original_pos[new_pos] = original_pos[old_pos]
+            original_pos[old_pos] = tempP
+            old_pos = new_pos
+
 
 pos_0 = encrypted_file.index(0)
 
 print(encrypted_file)
 
-print(encrypted_file[(pos_0+1000)%len(encrypted_file)], encrypted_file[(pos_0+2000)%len(encrypted_file)], encrypted_file[(pos_0+3000)%len(encrypted_file)], (encrypted_file[(pos_0+1000)%len(encrypted_file)]+encrypted_file[(pos_0+2000)%len(encrypted_file)]+encrypted_file[(pos_0+3000)%len(encrypted_file)]) )
+print(encrypted_file[(pos_0+1000)%len_file], encrypted_file[(pos_0+2000)%len_file], encrypted_file[(pos_0+3000)%len_file], (encrypted_file[(pos_0+1000)%len_file]+encrypted_file[(pos_0+2000)%len_file]+encrypted_file[(pos_0+3000)%len_file]) )
 
 # Nâo é 21743 (tá muito alto)
 # Não é 12570 (tá muito alto)
